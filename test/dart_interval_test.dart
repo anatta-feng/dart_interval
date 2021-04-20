cdcd4be45fa5cf67ce1febf45c6faa5758a386b3
渣渣辉 <1@fxcdev.com>
2021-04-20 21:59:23 +0800

    fix: fix test case.

--- test/dart_interval_test.dart
+++ test/dart_interval_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 void main() {
   test("test Interval", () {
     var v1 = Interval.between(1, 3);
-    expect(v1.toString(), '[1, 3]');
+    expect(v1.toString(), '(1, 3)');
   });
 
   test("test Interval merging", () {
@@ -12,20 +12,20 @@ void main() {
     var v2 = Interval.between(2, 4);
     var v3 = Interval.between(6, 8);
     var intervalSet = IntervalSet([v1, v2, v3]);
-    expect(intervalSet.toString(), '[1, 4],[6, 8]');
+    expect(intervalSet.toString(), '(1, 4),(6, 8)');
   });
 
   test("test Interval intersection", () {
     var v1 = Interval.between(1, 3);
     var v2 = Interval.between(2, 4);
     var result = v1 & v2;
-    expect(result.toString(), "[2, 3]");
+    expect(result.toString(), "(2, 3)");
   });
   
   test("test Interval union", () {
     var v1 = Interval.between(1, 3);
     var v2 = Interval.between(2, 4);
     var result = v1 | v2;
-    expect(result.toString(), "[1, 4]");
+    expect(result.toString(), "(1, 4)");
   });
 }
